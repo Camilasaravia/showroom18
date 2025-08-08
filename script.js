89452f82b31bd11a0af3c05cddb817cd94af7aeb
Camilasaravia <saraviacamila15@gmail.com>
2025-08-07 21:22:56 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const botones = document.querySelectorAll('.btn');
-
-botones.forEach((boton) => {
-  boton.addEventListener('click', () => {
-    alert("Producto agregado al carrito 🛒");
-  });
-});
